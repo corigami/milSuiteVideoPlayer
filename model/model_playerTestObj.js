1d9ca3944e841e280bfc4a85ec5af3c3a7faf4e7
Corey Willinger <corey.willinger@gmail.com>
2017-02-13 08:46:14 -0500

    chore: created model objects to hold lesson data

---
+++ model/model_playerTestObj.js
@@ -0,0 +1,159 @@
+    var LESSONS = [   
+        {
+            title: "Test Group",
+            module: [{
+                text: "Test Track",
+                link: "Slide 10.mp4",
+                milTube: "test.vtt"
+            }, {
+                text: "Test Track2",
+                link: "Slide 10.mp4",
+                milTube: "test2.vtt"
+            }]
+        },
+        {
+            title: "Intro to Software Testing",
+            module: [{
+                text: "Purpose of Software Testing",
+                link: "03112016-134817-1071365442-1-349.mp4",
+                milTube: "11553"
+            }, {
+                text: "Errors, Faults and Failures",
+                link: "03112016-135012-116208741-1-281.mp4",
+                milTube: "11554"
+            }, {
+                text: "Software Quality",
+                link: "03112016-135224-1809607656-1-441.mp4",
+                milTube: "11555"
+            }, {
+                text: "Categories, Types, Levels, and Limits of Testing",
+                link: "03252016-133625-763221133-1-489.mp4",
+                milTube: "11718"
+            }, {
+                text: "Verification and Validation",
+                link: "03252016-133656-594125888-1-357.mp4",
+                milTube: "11719"
+            }]
+        }, {
+            title: "Product Reviews and QA",
+            module: [{
+                text: "Software QA and Testing",
+                link: "03252016-134406-730158133-1-301.mp4",
+                milTube: "11720"
+            }, {
+                text: "Types of Faults",
+                link: "03252016-134526-110651402-1-431.mp4",
+                milTube: "11721"
+            }, {
+                text: "Fault Handling Approaches and Economics",
+                link: "03252016-134826-569896349-1-600.mp4",
+                milTube: "11722"
+            }, {
+                text: "Code Reviews",
+                link: "03252016-134942-1313563431-1-579.mp4",
+                milTube: "11723"
+            }, {
+                text: "Effectiveness and Applicability to Pre-code Artifacts",
+                link: "03252016-135109-629200385-1-600.mp4",
+                milTube: "11724"
+            }]
+        }, {
+            title: "Testing Strategies",
+            module: [{
+                text: "Setting the Stage",
+                link: "04042016-100745-516838468-1-515.mp4",
+                milTube: "11790"
+            }, {
+                text: "Unit Testing",
+                link: "07182016-112102-913622322-1-600.mp4",
+                milTube: "13103"
+            }, {
+                text: "Integration Testing",
+                link: "07182016-113810-292244187-1-600.mp4",
+                milTube: "13105"
+            }, {
+                text: "System Testing",
+                link: "04042016-111908-1015763518-1-379.mp4",
+                milTube: "11792"
+            }, {
+                text: "Acceptance Testing",
+                link: "07192016-102425-1832062724-1-600.mp4",
+                milTube: "13165"
+            }, {
+                text: "Regression and Installation Testing",
+                link: "04042016-112128-2064602769-1-385.mp4",
+                milTube: "11794"
+            }, {
+                text: "IV and When to Stop",
+                link: "04042016-112219-1653115425-1-583.mp4",
+                milTube: "11795"
+            }]
+        }, {
+            title: "Functional Testing",
+            module: [{
+                text: "Boundary Value Analysis",
+                link: "04042016-112537-943794841-1-600.mp4",
+                milTube: "11796"
+            }, {
+                text: "Equivalence Partioning",
+                link: "07192016-110049-559763040-1-600.mp4",
+                milTube: "13244"
+            }, {
+                text: "Decision Tables",
+                link: "04042016-112722-1007659156-1-600.mp4",
+                milTube: "11798"
+            }, {
+                text: "Other Techniques",
+                link: "04042016-112821-918188658-1-600.mp4",
+                milTube: "11799"
+            }]
+        }, {
+            title: "Structural Testing",
+            module: [{
+                text: "Code Coverage",
+                link: "04062016-124535-503542870-1-600.mp4",
+                milTube: "11840"
+            }, {
+                text: "Data FLow Coverage",
+                link: "04062016-124622-1007040509-1-536.mp4",
+                milTube: "11841"
+            }, {
+                text: "Function Coverage",
+                link: "04062016-124658-681000776-1-600.mp4",
+                milTube: "11842"
+            }]
+        }, {
+            title: "Test Planning and Execution",
+            module: [{
+                text: "Assessing Software Quality Risk",
+                link: "04052016-084544-1273110561-1-600.mp4",
+                milTube: "11805"
+            }, {
+                text: "Developing A Test Plan",
+                link: "04052016-084707-2066994483-1-600.mp4",
+                milTube: "11806"
+            }, {
+                text: "Managing Test Execution",
+                link: "04052016-084846-507818581-1-600.mp4",
+                milTube: "11807"
+            }, {
+                text: "Bug Tracking",
+                link: "04052016-085057-576888042-1-600.mp4",
+                milTube: "11810"
+            }]
+        }, {
+            title: "Software Testing and Tools",
+            module: [{
+                text: "Software Testing Tools",
+                link: "04072016-085613-174872497-1-600.mp4",
+                milTube: "11863"
+            }]
+        }, {
+            title: "Test Driven Development",
+            module: [{
+                text: "TDD Example",
+                link: "08222016-111615-147696729-1-692.mp4",
+                milTube: "13537"
+            }]
+        }
+    ];
