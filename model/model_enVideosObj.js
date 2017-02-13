40fa88114fb5fbdec64adc24976915713d50e0d4
Corey Willinger <corey.willinger@gmail.com>
2017-02-13 13:58:03 -0500

    chore: updated links for AFIT en model.

--- model/model_enVideosObj.js
+++ model/model_enVideosObj.js
@@ -1,10 +1,70 @@
     var LESSONS = [
         {
-            title: "No title yet",
+            title: "Requirements",
             module: [{
-                text: "No video title yet",
-                link: "no link yet",
-                milTube: "no miltub number yet"
+                text: "Engineering Resilience into AF Systems",
+                link: "02102017-155113-245170002-1-306.mp4",
+                milTube: "15185"
+            },{
+                text: "Ethnography in Requirements Engineering",
+                link: "02102017-154329-279217441-1-600.mp4",
+                milTube: "15181"
+            }]
+        },{
+            title: "Design",
+            module: [{
+                text: "No videos yet",
+                link: "#",
+                milTube: "#"
+            }]
+        },{
+            title: "Implementation",
+            module: [{
+                text: "No videos yet",
+                link: "#",
+                milTube: "#"
+            }]
+        },{
+            title: "Testing",
+            module: [{
+                text: "No videos yet",
+                link: "#",
+                milTube: "#"
+            }]
+        },{
+            title: "Maintenance",
+            module: [{
+                text: "Developing a Custom Software Maintenance Model",
+                link: "02102017-153041-115709483-1-600.mp4",
+                milTube: "15182"
+            }]
+        },
+        {
+            title: "Agile",
+            module: [{
+                text: "Integrating Agile",
+                link: "02102017-155608-2009788801-1-368.mp4",
+                milTube: "15184"
+            },{
+                text: "Agile in the DoD",
+                link: "02102017-154830-1445243687-1-109.mp4",
+                milTube: "15184"
+            }]
+        },
+        {
+            title: "Other",
+            module: [{
+                text: "AF Insider Threat System Development",
+                link: "02102017-154639-60323132-1-600.mp4",
+                milTube: "15186"
+            },{
+                text: "SWE Abuse Cases 1",
+                link: "12212016-143408-1005928052-1-600.mp4",
+                milTube: "14663"
+            },{
+                text: "Search Based SWE",
+                link: "12212016-143025-733077140-1-374.mp4",
+                milTube: "14659"
             }]
         }
     ];
