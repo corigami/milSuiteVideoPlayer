b443017f23797a6cdde893d2199c07069a2fee7c
Corey Willinger <corey.willinger@gmail.com>
2017-02-13 08:48:18 -0500

    chore: stripped data from enVideoObj

--- model/model_enVideosObj.js
+++ model/model_enVideosObj.js
@@ -1,26 +1,10 @@
     var LESSONS = [
         {
-            title: "Intro to Software Testing",
+            title: "No title yet",
             module: [{
-                text: "Purpose of Software Testing",
-                link: "03112016-134817-1071365442-1-349.mp4",
-                milTube: "11553"
-            }, {
-                text: "Errors, Faults and Failures",
-                link: "03112016-135012-116208741-1-281.mp4",
-                milTube: "11554"
-            }, {
-                text: "Software Quality",
-                link: "03112016-135224-1809607656-1-441.mp4",
-                milTube: "11555"
-            }, {
-                text: "Categories, Types, Levels, and Limits of Testing",
-                link: "03252016-133625-763221133-1-489.mp4",
-                milTube: "11718"
-            }, {
-                text: "Verification and Validation",
-                link: "03252016-133656-594125888-1-357.mp4",
-                milTube: "11719"
+                text: "No video title yet",
+                link: "no link yet",
+                milTube: "no miltub number yet"
             }]
         }
     ];
