e4d88029dd2dceb053a9cf2312730a2c91542d3f
Corey Willinger <corey.willinger@gmail.com>
2017-02-23 09:04:45 -0500

    feat: added another model for ISW

---
+++ model/model_iswObj.js
@@ -0,0 +1,23 @@
+    var LESSONS = [{
+            title: "milSuite 101",
+            module: [{
+                    text: "milSuite Intro and Motivation",
+                    link: "02172017-173735-2097089956-1-600.mp4",
+                    milTube: "15303",
+                    description: "Why the AF / AFIT / LS should use milSuite."
+                },
+                {
+                    text: "milSuite Basics",
+                    link: "02172017-183610-1858747630-1-600.mp4",
+                    milTube: "15305",
+                    description: "What are the major components of milSuite and how can I use them?"
+                },
+                {
+                    text: "milSuite Advanced",
+                    link: "02222017-202205-768381746-1-600.mp4",
+                    milTube: "15319",
+                    description: "How to set up a milBook Group, and adjust group settings"
+                }
+            ]
+        }
+    ];
